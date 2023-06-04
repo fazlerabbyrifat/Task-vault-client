3634b02527e7d6eea5835512dd45cb6e071fea30
fazlerabbyrifat <rifatfazlerabby@gmail.com>
2023-06-04 20:26:17 +0600

    banner added in home oage

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import banner from '../../../assets/banner.jpg'
+
+const Banner = () => {
+  return (
+    <div className="">
+      <div className="max-w-screen-xl bg-white shadow-lg rounded-lg">
+        <div className="md:flex gap-10">
+          <div className="w-1/2">
+            <img
+              src={banner}
+              alt="Banner Image"
+              className="h-full w-full"
+            />
+          </div>
+          <div className="w-1/2 p-6 flex flex-col justify-center ">
+            <h1 className="text-2xl font-bold mb-4">Welcome to Task Vault</h1>
+            <p className="text-gray-700">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              fringilla felis at lacus bibendum, a cursus erat placerat. Fusce
+              lobortis nunc sed neque commodo, non pulvinar arcu posuere. Sed
+              auctor velit a vestibulum feugiat.
+            </p>
+            <button className="btn btn-accent btn-outline w-1/3 mt-5">See More</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/pages/Shared/Navbar.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from '../../assets/logo.jpeg'
+
+const Navbar = () => {
+    const navItems = <>
+    <li>
+        <Link to='/'>Home</Link>
+    </li>
+    <li>
+        <Link to='/'>My Tasks</Link>
+    </li>
+    <li>
+        <Link to='/'>About</Link>
+    </li>
+    <li>
+        <Link to='/'>Blogs</Link>
+    </li>
+    </>
+  return (
+    <div className="navbar fixed z-30 opacity-70 max-w-screen-xl bg-black text-white">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navItems}
+          </ul>
+        </div>
+        <div className="flex">
+            <img className="w-16" src={logo} alt="" />
+            <Link className="btn btn-ghost normal-case text-3xl font-bold text-teal-500">Task Vault</Link>
+        </div>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          {navItems}
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <button className="btn btn-accent text-black">Get Started</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
